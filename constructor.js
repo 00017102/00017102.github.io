9d819bf8e41621dedc69171102e91b0ed43f28a5
Alisher <aisroilov1029@gmail.com>
2023-11-18 15:15:27 +0500

    calculating price nearly finished

--- constructor.js
+++ constructor.js
@@ -1,7 +1,15 @@
 
 document.addEventListener('DOMContentLoaded', function() {
+    showCustomFlavorInput();
     showCustomWriting();
+    document.getElementById('cakeSize').addEventListener('change', updatePrice);
+    document.getElementById('yesWriting').addEventListener('change', updatePrice);
+    document.getElementById('noWriting').addEventListener('change', updatePrice);
+    document.getElementById('cakeFlavor').addEventListener('change', updatePrice);
     updatePrice();
+    document.querySelectorAll('input[name="topping"]').forEach(function(elem){
+        elem.addEventListener('change', updatePrice);
+    });
 });
 
 function showCustomWriting(){
@@ -18,35 +26,81 @@ function showCustomWriting(){
         });
     });
 }
+function showCustomFlavorInput() {
+    const selectedFlavor = document.getElementById('cakeFlavor').value;
+    const customFlavorDiv = document.getElementById('customFlavor');
+
+    if (selectedFlavor === 'other') {
+        customFlavorDiv.style.display = 'block';
+    } else {
+        customFlavorDiv.style.display = 'none';
+    }
+}
+
+// Event listener for flavor selection
+document.getElementById('cakeFlavor').addEventListener('change', function() {
+    showCustomFlavorInput();
+    updatePrice(); // Assuming you have an updatePrice function for price calculation
+});
+
+
 
 //calculate and update the price
 function updatePrice() {
     //initial price
     let totalPrice = 0;
+    const toppingPrice = 5000;
     //additional price for custom writing
     const customWritingPrice = 50000;
     //get selected size
     const selectedSize = document.getElementById('cakeSize').value;
     //get custom writing
     const customWritingSelected = document.getElementById('yesWriting').checked;
+    //get selected flavor
+    const selectedFlavor = document.getElementById('cakeFlavor').value;
     //prices for each size
     const sizePrices = {
         none: 0,
-        bento: 100000,
-        small: 200000,
-        medium: 300000,
-        large: 400000,
-        xlarge: 500000
+        bento: 80000,
+        small: 100000,
+        medium: 200000,
+        large: 300000,
+        xlarge: 400000
+    };
+    //price for each flavor
+    const flavorPrices = {
+        none: 0,
+        vanilla: 30000,
+        chocolate: 35000,
+        redVelvet: 40000,
+        lemon: 45000,
+        banana: 50000,
+        cheesecake: 55000,
+        coffee: 60000,
+        coconut: 65000,
+        fruit: 70000,
+        pistachio: 75000,
+        peanut: 80000,
+        caramel: 85000,
+        blackForest: 90000,
+        strawberry: 95000,
+        other: 100000 
     };
 
-    
     //update price accroding to size
     totalPrice += sizePrices[selectedSize];
-
-    //check if custom writing is added
-    if (customWritingSelected) {
-        //update price if yes
-        totalPrice += customWritingPrice;
+    //update price according to flavour
+    totalPrice += flavorPrices[selectedFlavor];
+    //calculate price for toppings
+    if(selectedFlavor != 'none' && selectedSize != 'none'){
+        const toppings = document.querySelectorAll('input[name="topping"]:checked');
+        const numberOfToppings = toppings.length;
+        totalPrice += numberOfToppings * toppingPrice;
+        //check if custom writing is added
+        if (customWritingSelected) {
+            //update price if yes
+            totalPrice += customWritingPrice;
+        }
     }
     //format srice
     function formatPrice(totalPrice){
@@ -55,7 +109,4 @@ function updatePrice() {
 
     //update price display
     document.getElementById('cakePrice').innerHTML = `${formatPrice(totalPrice)} sum`;
-    document.getElementById('cakeSize').addEventListener('change', updatePrice);
-    document.getElementById('yesWriting').addEventListener('change', updatePrice);
-    document.getElementById('noWriting').addEventListener('change', updatePrice);
 }
